7a300d814c77b3d0f4ff648d9cf6e2733444ead8
fabianoheizer <fabianoheizer@gmail.com>
2020-04-19 20:14:31 -0300

    HeroService provider para injetar no app e Obervable(rxjs), Componente MessageService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +11,26 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-    heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
 
-    constructor() { }
+    // When Angular creates a HeroesComponent, the Dependency Injection system sets the heroService parameter to the singleton instance of HeroService.
+    constructor(
+        private heroService: HeroService,
+        private messageService: MessageService
+    ) { }
 
-    ngOnInit() {}   
+    ngOnInit() {
+        this.getHeroes();
+    }   
         
     onSelect(hero: Hero){
         this.selectedHero = hero;
+        this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
     }
 
 }
